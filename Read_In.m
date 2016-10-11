7fb6b2bff3d403c6cf2982ac313f089bc185d705
Jas1238Smith <jas1238smith@gmail.com>
2016-10-11 19:48:18 -0400

    Add files via upload
    Function to read in open/closed audio samples, trim the length between 10 and 30 seconds, and save a txt file of tempo information if it exists.

---
+++ Read_In.m
@@ -0,0 +1,48 @@
+function [audiosamp,audiotxt] = Read_In()
+%READ_IN: Prompts user to select and trim audio example
+
+% Select Audio Sample
+audiotxt = [];
+
+good = 0;
+while good == 0;
+OC = input('Open or Closed? ','s');
+if strcmp(OC,'Open')
+    OC = 'open';
+end
+if strcmp(OC,'Closed')
+    OC = 'closed';
+end
+if (strcmp(OC,'open') == 1 || strcmp(OC,'closed') == 1) 
+    good = 1;
+end
+end
+
+Number = 26;
+while (Number >= 25 || Number < 1)
+Number = input('Enter Sample Number(1-25): ');
+end
+Number = int2str(Number);
+while(size(Number) < 3)
+    Number = strcat('0',Number);
+end
+
+fA = strcat(OC,'_',Number,'.wav');
+Filename = strcat('training_set\',OC,'\',fA);
+[y,Fs] = audioread(Filename);
+
+if strcmp(OC,'open')
+    fB = strcat(OC,'_',Number,'.txt');
+    Filename2 = strcat('training_set\',OC,'\',fB);   
+    audiotxt = csvread(Filename2);
+end
+
+% Trim Audio
+s = 31;
+while (s > 30 || s < 1)
+s = input('Input Sound Length in Seconds(10-30): ');
+end
+audiosamp = y(1:s*Fs);
+
+end
+
